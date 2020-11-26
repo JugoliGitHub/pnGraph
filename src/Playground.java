7137b7a8d620893acd82b898155eef553dd8ed74
julian <julian.mueller1@uni-oldenburg.de>
2020-11-26 21:07:58 +0100

    add playground for easier access to own created petrinets

--- src/Playground.java
+++ src/Playground.java
@@ -1,10 +1,18 @@
 import exception.WrongDimensionException;
+import graph.Vector;
 
+/**
+ * This class is for fast testing inside my IDE.
+ */
 public class Playground {
 
   public static void main(String[] args) throws WrongDimensionException {
-    Petrinet petrinet = PetriReader.createPetriNetAndMarkings("s1:t1;;t1:s1;;", "1");
+    Petrinet petrinet = PetriReader.createPetriNetAndMarkings("s1:t1;s2:;;t1:s2;;", "2,0");
     CoverabilityGraph covGraph = PetriReader.createCoverabilityGraph(petrinet, "covGraph");
+    petrinet.setVectors();
+    System.out.println(petrinet);
+    System.out.println(covGraph);
+    System.out.println(petrinet.getBoundedness());
   }
 
 }
